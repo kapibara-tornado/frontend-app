c049f54c2667fce5b433e2a3af494490c329fc65
baku.shoda <baku-shoda@ielove-group.jp>
2024-08-30 10:45:18 +0900

    add play page

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -7,28 +7,79 @@ import styled from 'styled-components';
 function Play() {
   return (
     <div>
-        <Wrapper
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 270, 270, 0],
-            borderRadius: [
-              '20%',
-              '20%',
-              '50%',
-              '50%',
-              '20%',
-            ],
-          }}
-        />
+      <Wrapper>
+        <Card>
+          <Question>
+            この質問はテストです。この質問はテストです。
+          </Question>
+        </Card>
+        <Buttons>
+          <BadButton>Bad</BadButton>
+          <GoodButton>Good</GoodButton>
+        </Buttons>
+      </Wrapper>
     </div>
   );
 }
 
 export default Play;
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: 120vh;
+  background-color: #f0ffff;
+  padding-top: 100px;
+  padding-bottom: 150px;
+`;
+
+const Card = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 40px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   text-align: center;
-  height: 100px;
-  width: 100px;
-  background-color: lightblue;
+  width: 100%;
+  max-width: 700px;
+  min-height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  word-break: break-all;
+`;
+
+const Question = styled.p`
+  font-size: 1.8rem;
+  color: #333;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 100px;
+  margin-top: 50px;
+`;
+const GoodButton = styled.button`
+  color: #fff;
+  font-weight: 800;
+  background-color: #3cb371;
+  border-radius: 50%;
+  line-height: 150px;
+  width: 150px;
+  height: 150px;
+  padding: 0;
+  font-size: 1.5rem;
+`;
+
+const BadButton = styled.button`
+  color: #fff;
+  font-weight: 800;
+  background-color: #dc143c;
+  border-radius: 50%;
+  line-height: 150px;
+  width: 150px;
+  height: 150px;
+  padding: 0;
+  font-size: 1.5rem;
 `;
