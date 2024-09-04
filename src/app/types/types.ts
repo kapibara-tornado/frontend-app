60de784d94cfe781e4f15d829c3ed6551a84b730
bakushoda <bshoda.ace@gmail.com>
2024-09-05 01:55:44 +0900

    add: ResultBackgroundImage

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,3 +1,6 @@
+import { ReactNode } from "react";
+
+//政治版mbtiカードコンポーネントのプロパティ定義
 export interface TypeInfo {
     id: number;
     typeAlphabet: string;
@@ -6,9 +9,16 @@ export interface TypeInfo {
     senseOfValue: string;
 }
 
+//似ている政治家カードコンポーネントのプロパティ定義
 export interface PoliticianInfo {
     id: number;
     politicianName: string;
     partyName: string;
     explanation: string;
 }
+
+//BackgroundImageコンポーネントのプロパティ定義
+export interface BackgroundImageProps {
+    id: number;
+    children: ReactNode;
+  }
