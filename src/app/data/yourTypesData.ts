d3aa9a0bd133bb1169a71ecbbb1c79e39680a083
bakushoda <bshoda.ace@gmail.com>
2024-09-10 21:10:46 +0900

    add: image result

--- src/app/data/yourTypesData.ts
+++ src/app/data/yourTypesData.ts
@@ -9,6 +9,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済成長と安定を最優先に考える。市場の自由を尊重しつつ、政府の強い統制を支持する。伝統的な価値観を重視し、保守的な政策を推進する。',
     senseOfValue: '経済成長、国家の安全保障、社会の安定',
+    characterImage: '/Types/EACN.jpg'
   },
   {
     id: 2,
@@ -17,6 +18,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済成長を重視し、国際的な協力や貿易を推進するが、強力な政府の統制を支持する。伝統的な価値観を保持しながら、国際的な影響力を拡大する。',
     senseOfValue: '経済成長、国際協力、保守的価値観',
+    characterImage: '/Types/EACI.jpg'
   },
   {
     id: 3,
@@ -25,6 +27,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由と成長を重視し、自由市場を支持する。自国のために新たな社会政策を推進するが、政府の役割は最小限に留める。',
     senseOfValue: '経済的自由、自国の安定、社会的進歩',
+    characterImage: '/Types/EALN.jpg'
   },
   {
     id: 4,
@@ -33,6 +36,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       ' 経済的自由を重視し、国際的な協力と貿易を推進する。新たな社会政策を支持し、政府の干渉を最小限に抑えつつ、グローバルな課題にも積極的に取り組む。',
     senseOfValue: '経済的自由、国際協力、リベラルな価値観',
+    characterImage: '/Types/EALI.jpg'
   },
   {
     id: 5,
@@ -41,6 +45,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '自由市場を支持し、経済的自由を最優先に考える。自国の安定と安全保障を重視し、保守的な価値観を持つが、政府の干渉は最小限に抑える。',
     senseOfValue: '自由市場、国家主権、保守的価値観',
+    characterImage: '/Types/ELCN.jpg'
   },
   {
     id: 6,
@@ -49,6 +54,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由と市場競争を重視し、国際的な協力を推進。伝統的な価値観を保持しつつ、国際社会との連携を強化する。',
     senseOfValue: '自由市場、国際協力、保守的価値観',
+    characterImage: '/Types/ELCI.jpg'
   },
   {
     id: 7,
@@ -57,6 +63,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由を重視し、自国の安定と主権を守ることを最優先に考える。新たな社会政策を推進しつつ、国家の安全保障を重視。',
     senseOfValue: '経済的自由、自国の安定、新たな価値観',
+    characterImage: '/Types/ELLN.jpg'
   },
   {
     id: 8,
@@ -65,6 +72,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由を支持し、国際的な協力と多国間の協定を推進。新たな価値観を持ち、社会の進歩と国際的な影響力の拡大を目指す。',
     senseOfValue: '自由市場、国際協力、新たな価値観',
+    characterImage: '/Types/ELLI.jpg'
   },
   {
     id: 9,
@@ -73,6 +81,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '社会的進歩と公正を推進しつつ、保守的な価値観を尊重し、自国の安定を重視する。強力な政府の統制を支持し、経済の安定と成長を目指す。',
     senseOfValue: '社会正義、国家主権、保守的価値観',
+    characterImage: '/Types/PACN.jpg'
   },
   {
     id: 10,
@@ -81,6 +90,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '社会的進歩と国際協力を推進しつつ、保守的な価値観を尊重。強力な政府の統制を支持し、国際社会における影響力の拡大を目指す。',
     senseOfValue: '社会正義、国際協力、保守的価値観',
+    characterImage: '/Types/PACI.jpg'
   },
   {
     id: 11,
@@ -89,6 +99,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '社会的進歩と新たな価値観を推進しつつ、自国の安定と主権を重視する。政府の役割を最小限に抑え、自由市場を支持する。',
     senseOfValue: '社会正義、国家主権、新たな価値観',
+    characterImage: '/Types/PALN.jpg'
   },
   {
     id: 12,
@@ -97,6 +108,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '社会的進歩と国際協力を推進し、新たな価値観を持ちながら、政府の干渉を最小限に抑える。グローバルな問題に積極的に取り組む。',
     senseOfValue: '社会正義、国際協力、新たな価値観',
+    characterImage: '/Types/PALI.jpg'
   },
   {
     id: 13,
@@ -105,6 +117,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由と市場競争を重視し、保守的な価値観と自国の安定を尊重する。政府の役割を最小限に抑え、社会の秩序を維持する。',
     senseOfValue: '自由市場、国家主権、保守的価値観',
+    characterImage: '/Types/PLCN.jpg'
   },
   {
     id: 14,
@@ -113,6 +126,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '経済的自由と市場競争を重視し、国際的な協力を推進しつつ、保守的な価値観を尊重する。政府の干渉を最小限に抑え、グローバルな課題に対応する。',
     senseOfValue: '自由市場、国際協力、保守的価値観',
+    characterImage: '/Types/PLCI.jpg'
   },
   {
     id: 15,
@@ -121,6 +135,7 @@ export const YourTypes: TypeInfo[] = [
     feature:
       '社会的進歩と新たな価値観を推進しつつ、自国の安定と主権を重視する。経済的自由と市場競争を支持し、政府の役割を最小限に抑える。',
     senseOfValue: '社会正義、国家主権、新たな価値観',
+    characterImage: '/Types/PLLN.jpg'
   },
   {
     id: 16,
@@ -129,5 +144,6 @@ export const YourTypes: TypeInfo[] = [
     feature:
       ' 社会的進歩と国際協力を推進しつつ、新しい価値観を重視する。経済的自由を支持し、政府の干渉を最小限に抑えつつ、グローバルな課題に対応する。',
     senseOfValue: '社会正義、国際協力、新たな価値観',
+    characterImage: '/Types/PLLI.jpg'
   },
 ];
