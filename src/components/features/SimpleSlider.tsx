3039e31f8b3f315ce77d94591157f29eab8b0c66
bakushoda <bshoda.ace@gmail.com>
2024-09-10 13:22:24 +0900

    add: active dot

--- src/components/features/SimpleSlider.tsx
+++ src/components/features/SimpleSlider.tsx
@@ -1,19 +1,22 @@
 'use client';
 
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styled from 'styled-components';
 
 export const SimpleSlider = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    customPaging: () => <Dot />,  
+    beforeChange: (current: number, next: number) => setActiveSlide(next),
+    customPaging: (i: number) => <Dot active={i === activeSlide}/>,  
     dotsClass: "slick-dots custom-dots", 
   };
 
@@ -52,50 +55,31 @@ const SliderContainer = styled.div`
 
 const StyledSlider = styled(Slider)`
   .slick-list {
-    margin: 0 -10px; 
+    margin: 0 -10px;
   }
   
   .slick-slide > div {
-    padding: 0 10px; 
+    padding: 0 10px;
   }
 
   .slick-dots.custom-dots {
-    bottom: -500px; 
+    bottom: -500px;
   }
 
   .slick-dots.custom-dots li {
-    margin: 0 10px; 
-  }
-
-  .slick-dots.custom-dots li button {
-    &:before {
-      font-size: 16px; 
-      color: #ddd; 
-      opacity: 1;
-      transition: color 0.3s ease;
-    }
-  }
-
-  .slick-dots.custom-dots li.slick-active button:before {
-    color: #333; 
-  }
-
-  .slick-dots.custom-dots li.slick-active button:before {
-    color: #fff; 
-    background-color: #333; 
-    border-radius: 50%;
+    margin: 0 10px;
   }
 `;
 
-const Dot = styled.div`
-  width: 20px; 
-  height: 20px; 
-  background-color: #ddd; 
-  border-radius: 50%; 
+const Dot = styled.div<{ active: boolean }>`
+  width: 20px;
+  height: 20px;
+  background-color: ${({ active }) => (active ? '#333' : '#ddd')}; 
+  border-radius: 50%;
   cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #333;
+    background-color: #333; 
   }
 `;
