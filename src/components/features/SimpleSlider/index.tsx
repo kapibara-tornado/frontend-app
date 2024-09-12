635ed363b7a29fd01a12c286e7afba83ddfd7167
yoshi-non <himekawa.yoshino44@gmail.com>
2024-09-12 16:43:47 +0900

    fix: スライドのバグとコードリファクタリング

---
+++ src/components/features/SimpleSlider/index.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styled from 'styled-components';
+import { ResultDetailArea } from '../ResultDetailArea';
+import { useResult } from '@/usecases/useResult';
+import { ScoreBar } from '../ScoreBar';
+import {
+  determineIdsBasedOnScores,
+  getDirection,
+  getScore,
+} from './logics';
+
+export const SimpleSlider = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const { parsedScores } = useResult();
+
+  const idsForResultDetailArea =
+    determineIdsBasedOnScores(parsedScores);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (current: number, next: number) =>
+      setActiveSlide(next),
+    customPaging: (i: number) => (
+      <Dot active={i === activeSlide ? 'true' : 'false'} />
+    ),
+    dotsClass: 'slick-dots custom-dots',
+  };
+
+  return (
+    <SliderContainer>
+      <StyledSlider {...settings}>
+        {parsedScores &&
+          Array.from({ length: 4 }).map((_, index) => (
+            <div key={index}>
+              <ResultDetailArea
+                id={idsForResultDetailArea[index]}
+              />
+              <ScoreBar
+                score={getScore(index, parsedScores)}
+                direction={getDirection(
+                  index,
+                  parsedScores
+                )}
+                id={index * 2 + 1}
+              />
+            </div>
+          ))}
+      </StyledSlider>
+    </SliderContainer>
+  );
+};
+
+const SliderContainer = styled.div`
+  max-width: 1000px;
+  height: 620px;
+  margin: 50px auto;
+  background-color: white;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  position: relative;
+`;
+
+const StyledSlider = styled(Slider)`
+  .slick-list {
+    margin: 0 -10px;
+  }
+
+  .slick-slide > div {
+    padding: 0 10px;
+  }
+
+  .slick-dots.custom-dots {
+    bottom: -50px;
+  }
+
+  .slick-dots.custom-dots li {
+    margin: 0 10px;
+  }
+`;
+
+const Dot = styled.div<{ active: 'true' | 'false' }>`
+  width: 20px;
+  height: 20px;
+  background-color: ${({ active }) =>
+    active === 'true' ? '#333' : '#ddd'};
+  border-radius: 50%;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #333;
+  }
+`;
---
+++ src/components/features/SimpleSlider/logics/index.ts
@@ -0,0 +1,83 @@
+import { Scores } from '@/types/types';
+
+export const getScore = (
+  i: number,
+  parsedScores: Scores
+) => {
+  switch (i) {
+    case 1:
+      return parsedScores.E > parsedScores.P
+        ? parsedScores.E
+        : parsedScores.P;
+    case 2:
+      return parsedScores.A > parsedScores.S
+        ? parsedScores.A
+        : parsedScores.S;
+    case 3:
+      return parsedScores.L > parsedScores.C
+        ? parsedScores.L
+        : parsedScores.C;
+    case 4:
+      return parsedScores.I > parsedScores.N
+        ? parsedScores.I
+        : parsedScores.N;
+    default:
+      return 1;
+  }
+};
+
+export const getDirection = (
+  i: number,
+  parsedScores: Scores
+) => {
+  switch (i) {
+    case 1:
+      return parsedScores.E > parsedScores.P
+        ? 'left'
+        : 'right';
+    case 2:
+      return parsedScores.A > parsedScores.S
+        ? 'left'
+        : 'right';
+    case 3:
+      return parsedScores.L > parsedScores.C
+        ? 'left'
+        : 'right';
+    case 4:
+      return parsedScores.I > parsedScores.N
+        ? 'left'
+        : 'right';
+    default:
+      return 'left';
+  }
+};
+
+export const determineIdsBasedOnScores = (
+  parsedScores: Scores | undefined
+) => {
+  if (!parsedScores) return [0, 0, 0, 0];
+
+  const ids = [];
+
+  if (parsedScores.E > parsedScores.P) {
+    ids.push(1);
+  } else {
+    ids.push(2);
+  }
+  if (parsedScores.A > parsedScores.S) {
+    ids.push(3);
+  } else {
+    ids.push(4);
+  }
+  if (parsedScores.L > parsedScores.C) {
+    ids.push(5);
+  } else {
+    ids.push(6);
+  }
+  if (parsedScores.I > parsedScores.N) {
+    ids.push(7);
+  } else {
+    ids.push(8);
+  }
+  return ids;
+};
