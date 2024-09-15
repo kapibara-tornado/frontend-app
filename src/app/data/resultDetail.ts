343948d1de3f8dac145b28250b23528c8d0814ab
bakushoda <bshoda.ace@gmail.com>
2024-09-16 02:34:56 +0900

    change: resultdetail text

--- src/app/data/resultDetail.ts
+++ src/app/data/resultDetail.ts
@@ -12,15 +12,15 @@ export const ResultDetail: ResultDetailInfo[] = [
     id: 2,
     genre: '経済の自由度',
     genreImage: '/result/type1.png',
-    yourType: 'P型（進歩的）',
+    yourType: 'P型（進歩的価値観派）',
     typeDescription:
-      '進歩的のあなたは、これはテストです。これはテストです。これはテストです。',
+      '進歩主義派なあなたは、経済ではなく、社会正義や平等、環境保護などの進歩的な価値観を重視する立場です。',
   },
   {
     id: 3,
     genre: '政府の役割',
     genreImage: '/result/type2.png',
-    yourType: 'A型（権威主義）',
+    yourType: 'A型（権威主義派）',
     typeDescription:
       '権威主義派のあなたは、強力な政府の統制や、規制を支持し、秩序や安全を優先する立場です。',
   },
@@ -28,40 +28,40 @@ export const ResultDetail: ResultDetailInfo[] = [
     id: 4,
     genre: '政府の役割',
     genreImage: '/result/type2.png',
-    yourType: 'L型（自由主義）',
+    yourType: 'L型（自由主義派）',
     typeDescription:
-      '自由主義派のあなたは、これはテストです。これはテストです。これはテストです。',
+      '自由主義派のあなたは、個人の自由や自己決定権を重視し、政府の干渉を最小限にすることを支持する立場です。',
   },
   {
     id: 5,
     genre: '社会の保守性',
     genreImage: '/result/type3.png',
-    yourType: 'L型（リベラル）',
+    yourType: 'L型（革新派）',
     typeDescription:
-      'リベラル派のあなたは、これはテストです。これはテストです。これはテストです。',
+      'リベラル派のあなたは、個人の権利や多様性を重視し、社会変革や革新を支持する立場です。',
   },
   {
     id: 6,
     genre: '社会の保守性',
     genreImage: '/result/type3.png',
-    yourType: 'C型（保守的）',
+    yourType: 'C型（保守派）',
     typeDescription:
-      '保守派のあなたは、これはテストです。これはテストです。これはテストです。',
+      '保守派のあなたは、伝統的な価値観や社会秩序を重視し、変化に対して慎重な立場です。',
   },
   {
     id: 7,
     genre: '国際関係',
     genreImage: '/result/type4.png',
-    yourType: 'I型（国際主義）',
+    yourType: 'I型（国際主義派）',
     typeDescription:
-      '国際主義派のあなたは、これはテストです。これはテストです。これはテストです。',
+      '国際主義派のあなたは、国際協力やグローバルな問題解決を重視し、他国との連携を積極的に支持する立場です。',
   },
   {
     id: 8,
     genre: '国際関係',
     genreImage: '/result/type4.png',
-    yourType: 'N型（愛国主義）',
+    yourType: 'N型（国家主義派）',
     typeDescription:
-      '愛国主義派のあなたは、これはテストです。これはテストです。これはテストです。',
+      '愛国主義派のあなたは、国家の利益や主権を優先し、他国との協力よりも自国の独立性を重視する立場です。',
   },
 ];
