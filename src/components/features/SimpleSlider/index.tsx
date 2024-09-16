296885c7e990c4f4676c5801a7853c08b211dc7f
bakushoda <bshoda.ace@gmail.com>
2024-09-16 20:23:32 +0900

    change: slider setting

--- src/components/features/SimpleSlider/index.tsx
+++ src/components/features/SimpleSlider/index.tsx
@@ -22,7 +22,7 @@ export const SimpleSlider = () => {
 
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
